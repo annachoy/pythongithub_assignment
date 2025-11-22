309201b73d5f6953e21ac6546bd978a95bc7d6b9
Anna Choy <annachoy@usf.edu>
2025-11-22 17:35:12 -0500

    Clean up code, add comments, finalize program

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
+#welcome message
 print("Welcome to my Python program!")
+#ask user for input
 hours = input("How many hours did you study today? ")
-hours = float(hours)
-weekly_hours = hours * 7
-print(f"You are on track to study {weekly_hours} hours this week.")
+#convert input and calculate result
+#convert input to number then calculate estimated weekly time
 try:
     hours = float(hours)
+    weekly_hours = hours * 7
+#display response
+print(f"You are on track to study {weekly_hours} hours this week.")
+
+#run if the user entered invalid number
 except ValueError:
     print("Please enter a valid number.")
-    exit()
-    
